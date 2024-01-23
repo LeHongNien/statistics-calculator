23e235f586f2117def37a708e1d3fe846d6b3f12
Le Hong Nien <lehongnien1@gmail.com>
2024-01-23 22:33:44 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length;
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
+  const median = array.length % 2 === 0;
 }
 
 const calculate = () => {
