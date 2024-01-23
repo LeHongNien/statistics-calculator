7bb4b19ddfd3ba726d6560876b880c86596a39f5
Le Hong Nien <lehongnien1@gmail.com>
2024-01-23 22:32:09 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMedian = (array) => {
+
+}
 
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  }
+}
