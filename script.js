c35404ec726cf44201099b6fad1febb8dcbd2fa2
Le Hong Nien <lehongnien1@gmail.com>
2024-01-24 11:06:50 +0800

    update mode

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
-  const median = array.length % 2 === 0 ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]]): sorted[Math.floor(array.length / 2)];
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
   return median;
 }
 
-const calculate = () => {
-    const value = document.querySelector("#numbers").value;
-    const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
 }
