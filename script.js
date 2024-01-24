71d419fe99988f2f9899572390e1ac8a786fcfae
Le Hong Nien <lehongnien1@gmail.com>
2024-01-24 11:26:47 +0800

    update variance

--- script.js
+++ script.js
@@ -29,3 +29,13 @@ const getMode = (array) => {
 const getRange = (array) => {
   return Math.max(...array) - Math.min(...array);
 }
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance = array.reduce((acc, el) => {
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
+}
