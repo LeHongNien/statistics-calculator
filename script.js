3fd56dc40428d73387168263a2e663462fc80ddf
Le Hong Nien <lehongnien1@gmail.com>
2024-01-24 11:37:34 +0800

    update standardDeviation

--- script.js
+++ script.js
@@ -39,3 +39,29 @@ const getVariance = (array) => {
   }, 0) / array.length;
   return variance;
 }
+
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
+}
+
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
+
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
+}
